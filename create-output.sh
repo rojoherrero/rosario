70bc884441c50b292d104457db2f153af9d4ea2d
rojoherrero <sergio@rojoherrero.es>
2019-07-15 20:15:57 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-pandoc -s rosario.md -o output/rosario-pandoc.epub
 pandoc -s rosario.md -o output/rosario-pandoc.pdf
 pandoc -s rosario.md -o output/rosario-pandoc.tex
+pandoc -s rosario.md -o output/rosario-pandoc.html --ascii
+
