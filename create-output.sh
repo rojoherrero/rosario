284c3d79041303c282567f49f1f86aaca3b53587
rojoherrero <sergio@rojoherrero.es>
2019-07-15 22:54:29 +0200

    output script tweaked

--- create-output.sh
+++ create-output.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pandoc -s rosario.md -o output/rosario-pandoc.pdf
-pandoc -s rosario.md -o output/rosario-pandoc.tex
-pandoc -s rosario.md -o output/rosario-pandoc.html --ascii
-
+pandoc -s rosario.md -o output/rosario-pandoc.pdf -s
+#pandoc -s rosario.md -o output/rosario-pandoc.tex -s
+pandoc -s rosario.md -o output/rosario-pandoc.html --ascii -s
