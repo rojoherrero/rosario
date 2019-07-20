7fc5b5eada97070bbb6fdafc13717bd4581ed121
rojoherrero <sergio@rojoherrero.es>
2019-07-20 18:53:19 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
-pandoc -s rosario.md -o output/rosario-pandoc.pdf -s
-#pandoc -s rosario.md -o output/rosario-pandoc.tex -s
-pandoc -s rosario.md -o output/rosario-pandoc.html --ascii -s
+rm -rf output/*;
+current_date_time="`date +%Y%m%d%H%M%S`";
+echo Generating markdown;
+pandoc -s rosario.tex -o output/${current_date_time}_rosario.md;
+echo markdown generated;
+echo Generating HTML;
+pandoc -s rosario.tex -o output/${current_date_time}_rosario.html --ascii;
+echo HTML generated;
+echo Compiling to PDF;
+pdflatex -output-directory=output/ -jobname=${current_date_time}_rosario rosario.tex > /dev/null 2>&1;
+rm -rf output/${current_date_time}_rosario.aux output/${current_date_time}_rosario.out;
+echo PDF generated;
+echo DONE;
