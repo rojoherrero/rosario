a83276ded2a2b52437ca52bb48278d7fd1c12b67
rojoherrero <sergio@rojoherrero.es>
2019-07-30 23:48:33 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 rm -r output/*;
 current_date_time="`date +%Y%m%d%H%M%S`";
-echo Generating ODT;
-pandoc rosario.md -s -o output/${current_date_time}_rosario.odt;
-echo ODT generated
-echo Generating HTML;
-pandoc rosario.md -s -o output/${current_date_time}_rosario.html --ascii;
-echo HTML generated;
-echo Generating MOBI
-pandoc rosario.md -s -o output/temp_rosario.epub;
-ebook-convert output/temp_rosario.epub output/${current_date_time}_rosario.mobi;
-echo MOBI generated
+echo Generating EBOOKS
+pandoc rosario.md -s -o output/${current_date_time}_rosario.epub;
+ebook-convert output/${current_date_time}_rosario.epub output/${current_date_time}_rosario.mobi;
+echo EBOOKS generated
 echo Generating PDF;
-ebook-convert output/temp_rosario.epub output/${current_date_time}_rosario.pdf;
+ebook-convert output/${current_date_time}_rosario.epuboutput/${current_date_time}_rosario.pdf;
 echo PDF generated;
-rm output/temp_rosario.epub;
