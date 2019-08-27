4c063d9e442765402c7aa625977226ce942d28ca
rojoherrero <sergio@rojoherrero.es>
2019-08-27 23:46:46 +0200

    pandoc changed for asciidoctor

--- create-output.sh
+++ create-output.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 rm -r output/*;
 current_date_time="`date +%Y%m%d%H%M%S`";
-pandoc rosario.md --self-contained --standalone --ascii --table-of-contents --css style.css -o output/${current_date_time}_rosario.html;
+asciidoctor -o output/${current_date_time}_rosario.html base.adoc
 echo Generating EBOOKS
-#pandoc rosario.md --self-contained --standalone -o output/${current_date_time}_rosario.epub;
 ebook-convert output/${current_date_time}_rosario.html output/${current_date_time}_rosario.epub;
 ebook-convert output/${current_date_time}_rosario.html output/${current_date_time}_rosario.mobi;
 echo EBOOKS generated
