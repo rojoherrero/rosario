a5faeffeda6eef3618606de0d6681456d37e8f98
rojoherrero <sergio@rojoherrero.es>
2019-08-28 21:08:50 +0200

    refactoring

--- create-output.sh
+++ create-output.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -r output/*;
-current_date_time="`date +%Y%m%d%H%M%S`";
-asciidoctor -o output/${current_date_time}_rosario.html base.adoc
-echo Generating EBOOKS
-ebook-convert output/${current_date_time}_rosario.html output/${current_date_time}_rosario.epub;
-ebook-convert output/${current_date_time}_rosario.html output/${current_date_time}_rosario.mobi;
-echo EBOOKS generated
+current_date_time="`date +%Y%m%d`";
+asciidoctor -o output/${current_date_time}_devocionario.html devocionario.adoc;
+echo Generating EBOOKS;
+ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.epub;
+ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.mobi;
+echo EBOOKS generated;
 echo Generating PDF;
-ebook-convert output/${current_date_time}_rosario.html output/${current_date_time}_rosario.pdf;
+asciidoctor -r asciidoctor-pdf -b pdf -o output/${current_date_time}_devocionario.pdf devocionario.adoc;
 echo PDF generated;
