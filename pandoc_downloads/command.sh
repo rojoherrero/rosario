07a2847ed39ea4b616664d3e0615635a57a338f9
rojoherrero <sergio@rojoherrero.es>
2019-09-06 23:40:00 +0200

    wip

--- pandoc_downloads/command.sh
+++ pandoc_downloads/command.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-pandoc https://www.aciprensa.com/recursos/primer-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1410 -o pandoc_downloads/data/01_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/segundo-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1411/ -o pandoc_downloads/data/02_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/tercer-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1412/ -o pandoc_downloads/data/03_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/cuarto-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1413/ -o pandoc_downloads/data/04_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/quinto-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1414/ -o pandoc_downloads/data/05_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/sexto-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1415/ -o pandoc_downloads/data/06_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/septimo-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1417/ -o pandoc_downloads/data/07_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/octavo-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1418/ -o pandoc_downloads/data/08_natividad_virgen.adoc
-pandoc https://www.aciprensa.com/recursos/noveno-dia-de-la-novena-por-la-natividad-de-la-santisima-virgen-maria-1419/ -o pandoc_downloads/data/09_natividad_virgen.adoc
+pandoc https://www.devocionario.com/maria/rosario_10.html -o pandoc_downloads/data/luminosos.md
+pandoc https://www.devocionario.com/maria/rosario_4.html -o pandoc_downloads/data/gozosos.adoc
+pandoc https://www.devocionario.com/maria/rosario_5.html -o pandoc_downloads/data/dolorosos.adoc
+pandoc https://www.devocionario.com/maria/rosario_6.html -o pandoc_downloads/data/gloriosos.adoc
