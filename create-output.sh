4f1b7cff91845dabcc05d10001a26591e84bc9fc
rojoherrero <sergio@rojoherrero.es>
2019-09-21 17:55:14 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-rm -r output/*;
 current_date_time="`date +%Y%m%d`";
-asciidoctor -o output/${current_date_time}_devocionario.html devocionario.adoc;
-echo Generating EBOOKS;
-ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.epub;
-ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.azw3 --no-inline-toc;
-echo EBOOKS generated;
-echo Generating PDF;
-asciidoctor -r asciidoctor-pdf -b pdf -o output/${current_date_time}_devocionario.pdf devocionario.adoc;
-asciidoctor -r asciidoctor-pdf -b pdf -o output/${current_date_time}_rosario-viaje.pdf rosario-viaje.adoc;
-echo PDF generated;
+pdflatex -jobname=${current_date_time}_devocionario devocionario.tex
