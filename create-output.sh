b1185ce8ae9ff1034317dbbb963fc7d020f5dda6
rojoherrero <sergio@rojoherrero.es>
2019-09-22 13:34:06 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -6,7 +6,3 @@ echo Generating EBOOKS;
 ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.epub;
 ebook-convert output/${current_date_time}_devocionario.html output/${current_date_time}_devocionario.azw3 --no-inline-toc;
 echo EBOOKS generated;
-echo Generating PDF;
-asciidoctor -r asciidoctor-pdf -b pdf -o output/${current_date_time}_devocionario.pdf devocionario.adoc;
-asciidoctor -r asciidoctor-pdf -b pdf -o output/${current_date_time}_rosario-viaje.pdf rosario-viaje.adoc;
-echo PDF generated;
