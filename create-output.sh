b77b7384b7ca4e9189b413c1a375c8cadeb9f218
rojoherrero <sergio@rojoherrero.es>
2019-09-29 16:42:09 +0200

    wip

--- create-output.sh
+++ create-output.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+rm -f ./output/{*.aux,*.log,*.pdf,*.epub,*.azw3,*.html,*.dvi}
 current_date_time="`date +%Y%m%d`";
-pdflatex -jobname=${current_date_time}_devocionario devocionario.tex
+pdflatex -halt-on-error -output-directory=./output -output-format=pdf -jobname=${current_date_time}_devocionario devocionario.tex 
